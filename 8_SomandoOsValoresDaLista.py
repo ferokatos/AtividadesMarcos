06a72787f661646147aa38dbcef9e88bfcc0fe7f
ferokatos <gabrielholallan09@gmail.com>
2025-08-07 15:28:35 -0300

    (Resolução) 8-Somando os valores da lista

---
+++ 8_SomandoOsValoresDaLista.py
@@ -0,0 +1,4 @@
+def SomandoOsValoresDaLista(LST):
+    return sum(LST)
+soma = SomandoOsValoresDaLista([987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9])
+print(f'A soma total deu igual à {soma}')
