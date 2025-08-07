19e879a6fcbcee54ae0f3f8deb8b593d91e7d458
ferokatos <gabrielholallan09@gmail.com>
2025-08-07 15:34:46 -0300

    (Resolução) 15-Invertendo a lista

---
+++ 15_InvertendoALista.py
@@ -0,0 +1,6 @@
+def InvertendoALista(lista):
+    return lista[::-1]
+lista = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+listaInvertida = InvertendoALista(lista)
+print(f'Lista normal: {lista}')
+print(f'Lista invertida: {listaInvertida}')
