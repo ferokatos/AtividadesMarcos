ea832407a7de995a888414d3362fb724abe3bd02
ferokatos <gabrielholallan09@gmail.com>
2025-08-07 15:21:57 -0300

    (Resolução) 5-Encontrando os números impares

---
+++ 5_EncontrandoOsNumerosImpares.py
@@ -0,0 +1,8 @@
+def EncontrandoOsNumerosImpares(LST):
+    impares = []
+    for i in LST:
+        if i % 2 != 0:
+            impares.append(i)
+    return impares
+impares = EncontrandoOsNumerosImpares([987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9])
+print(f'Os números impares retirados da lista foram: {impares}')
